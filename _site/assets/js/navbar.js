87dd6c1b59b8acbcd536b63504ba25465f7953fd
sylhare <sylhareng@gmail.com>
2017-10-15 19:03:29 -0400

    trying new things navbar

--- _site/assets/js/navbar.js
+++ _site/assets/js/navbar.js
@@ -1,19 +1,19 @@
+function toggle() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
 $(window).scroll(function(){
     var h = 20;    
     var $scrollTop = $(window).scrollTop();
     
     if($scrollTop < 60){
-        $(".scrollbar").css({
+        $(".navbar").css({
           'paddingTop': h - ($scrollTop/3) + "px",
         });
     }
 });
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
