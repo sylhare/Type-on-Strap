9c3c882f271972430286402b191d2889e4e1d6c0
sylhare <sylhareng@gmail.com>
2017-10-28 19:17:12 -0400

    Responsive on scroll header images

--- _site/assets/js/navbar.js
+++ _site/assets/js/navbar.js
@@ -1,34 +1,35 @@
 /*
  * Display the menu items on smaller screens
  */
-$(function() {
-  var pull    = $('#pull');
-    menu    = $('nav ul');
-    menuHeight  = menu.height();
- 
-  $(pull).on('click', function(e) {
-    e.preventDefault();
-    menu.slideToggle();
-  });
+$(function () {
+    var pull = $('#pull');
+    menu = $('nav ul');
+    menuHeight = menu.height();
+
+    $(pull).on('click', function (e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
 });
 
 /*
  * Display the navbar back to normal after resize
  */
-$(window).resize(function(){
-  var w = $(window).width();
-  if(w > 320 && menu.is(':hidden')) {
-    menu.removeAttr('style');
-  }
-}); 
-
-$(window).scroll(function(){
-    var h = 20;    
-    var $scrollTop = $(window).scrollTop();
-    
-    if($scrollTop < 60){
-        $(".navbar").css({
-          'paddingTop': h - ($scrollTop/3) + "px",
-        });
+$(window).resize(function () {
+    var w = $(window).width();
+    if (w > 320 && menu.is(':hidden')) {
+        menu.removeAttr('style');
     }
+});
+
+
+/*
+$('.call-out').scroll(function () {
+    var x = $(this).scrollTop();
+    $(this).css('background-position', '0% ' + parseInt(-x / 10) + 'px');
+});
+*/
+$(window).scroll(function() {
+    var x = $(this).scrollTop();
+    $('#main').css('background-position', '100% ' + parseInt(-x / 1) + 'px' + ', 0% ' + parseInt(-x / 2) + 'px, center top');
 });
