2b6e7bc287f4027a37b6a5ebe229b6b54225f5fb
sylhare <sylhareng@gmail.com>
2018-05-09 19:29:40 -0400

    Vanilla JS with Masonry #74

---
+++ assets/js/masonry_init.js
@@ -0,0 +1,14 @@
+// init Masonry
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  gutter: '.gutter-sizer',
+  percentPosition: true
+});
+
+// layout Masonry after each image loads
+var imgLoad = imagesLoaded( elem );
+imgLoad.on( 'progress', function( instance, image ) {
+  msnry.layout();
+});
