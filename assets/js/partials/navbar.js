151a5ef6d0a4232faccb4606e41369feb16941d0
moong <gmun0929@gmail.com>
2018-06-21 20:17:32 +0900

    test

--- assets/js/partials/navbar.js
+++ assets/js/partials/navbar.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var menu = document.querySelector('nav ul');
 
     pull.addEventListener('click', function(e) {
-        menu.classList.toggle('hide');
+        menu.classList.toggle('block');
     });
 
     /*
@@ -28,5 +28,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var m = document.getElementById("main"), c = m.style;
         
         c.backgroundPosition = '100% ' + parseInt(-x/3) + 'px' + ', 0%, center top';
+        
+        var maxHeight = $("#site-header").height();
+	    var currentScroll = $(window).scrollTop();
+	    
+	    var menuOpacity = $("#site-header").css("opacity");
+	    
+	    if (maxHeight <= currentScroll) {
+	    	if(menuOpacity == 1){
+	    		$("#site-header").css("height","0");
+	    		$("#site-header").css("opacity","0");
+	    		$("#site-header").css("pointer-events","none");
+	    	}
+	    }else{
+	    	if(menuOpacity == 0){
+	    		$("#site-header").css("height","calc(6rem + 72px)");
+	    		$("#site-header").css("opacity","1");
+	    		$("#site-header").css("pointer-events","auto");
+	    	}
+	    }
     });
 });
