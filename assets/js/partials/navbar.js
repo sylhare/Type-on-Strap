3cc10a845490a720143bffb6ae8c901d83367f17
moong <gmun0929@gmail.com>
2018-06-21 20:30:51 +0900

    ㅅㄷㄴㅅ

--- assets/js/partials/navbar.js
+++ assets/js/partials/navbar.js
@@ -34,15 +34,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var maxHeight = $("#site-header").height();
 	    var currentScroll = $(window).scrollTop();
 	    
+	    
 	    var menuOpacity = $("#site-header").css("opacity");
 	    
 	    if (maxHeight <= currentScroll) {
+	    	console.log("크다")
 	    	if(menuOpacity == 1){
+	    		console.log("숨김")
 	    		$("#site-header").css("height","0");
 	    		$("#site-header").css("opacity","0");
 	    		$("#site-header").css("pointer-events","none");
 	    	}
 	    }else{
+	    	console.log("작다")
 	    	if(menuOpacity == 0){
 	    		$("#site-header").css("height","calc(6rem + 72px)");
 	    		$("#site-header").css("opacity","1");
