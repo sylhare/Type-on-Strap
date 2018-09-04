717eb2364de123bdec5b147e79d2b46401fd0949
sylhare <sylhareng@gmail.com>
2018-09-04 11:44:28 -0400

    Move the gulp files in assets

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,4 +1,7 @@
-// Use npm install to install all the dependencies located in package.json
+/* 
+ * Use `npm install` to install all the dependencies located in package.json 
+ * Then `gulp default` to minimize css and images.
+ */
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
@@ -11,53 +14,54 @@ const replace = require('gulp-replace');
 
 gulp.task('js', function () {
     gutil.log('... Minifying js');
-    gulp.src(['assets/js/partials/**.js'])
+    gulp.src(['js/partials/**.js'])
         .pipe(concat('main.min.js'))
         .pipe(uglify())
         .on('error', (err) => {
             gutil.log(gutil.colors.red('[Error]'), err.toString());
         })
-        .pipe(gulp.dest("assets/js/"))
+        .pipe(gulp.dest("js/"))
 });
 
 gulp.task("img", function () {
     gutil.log('... Minifying images');
-    gulp.src('assets/img/**/*.{png,svg,jpg,gif}')
+    gulp.src('img/**/*.{png,svg,jpg,gif}')
         .pipe(imagemin())
         .on('error', (err) => {
             gutil.log(gutil.colors.red('[Error]'), err.toString());
         })
-        .pipe(gulp.dest('assets/img/'))
+        .pipe(gulp.dest('img/'))
 });
 
 gulp.task('minify-bootstrap-css', function () {
     gutil.log('... Minifying isolated bootstrap');
-    gulp.src('assets/css/vendor/bootstrap-iso.css')
+    gulp.src('css/vendor/bootstrap-iso.css')
         .pipe(cssmin())
         .on('error', (err) => {
             gutil.log(gutil.colors.red('[Error]'), err.toString());
         })
         .pipe(concat('bootstrap-iso.min.css'))
-        .pipe(gulp.dest('assets/css/vendor/'));
+        .pipe(gulp.dest('css/vendor/'));
 })
 
 gulp.task("isolate-bootstrap-css", ['minify-bootstrap-css'], function () {
     gutil.log('... Generating isolated bootstrap');
-    gulp.src('assets/css/bootstrap-iso.less')
+    gulp.src('css/bootstrap-iso.less')
         .pipe(less())
         .pipe(replace('.bootstrap-iso html', ''))
         .pipe(replace('.bootstrap-iso body', ''))
-        .pipe(gulp.dest('assets/css/vendor/'));
+        .pipe(gulp.dest('css/vendor/'));
 });
 
 gulp.task("serve", function () {
     gutil.log('... Launching Web browser');
     gutil.log('... Starting Jelyll');
     shell.task([
-        "python -m webbrowser 'http://localhost:4000/Type-on-Strap/' && bundle exec jekyll serve --watch"
+        "python -m webbrowser 'http://localhost:4000/Type-on-Strap/'; cd .. && bundle exec jekyll serve --watch"
     ])
+    gutil.log('... If you still see this, it might not be working well');
 });
 
-gulp.task('default', ['js', 'img'], function () {
+gulp.task("default", ['js', 'img'], function () {
     return gutil.log('... Gulp is running!');
 });
