9eae038475afbc210252e44f64679e76e8ca3e40
sylhare <sylhare@outlook.com>
2018-12-09 16:35:18 -0500

    Fix #85 improve theme at low width

--- assets/js/partials/navbar.js
+++ assets/js/partials/navbar.js
@@ -1,22 +1,23 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function (event) {
 
-    /*
+   /*
     * Display the menu items on smaller screens
     */
     var pull = document.getElementById('pull');
     var menu = document.querySelector('nav ul');
 
-    pull.addEventListener('click', function(e) {
+    pull.addEventListener('click', function (e) {
         menu.classList.toggle('hide');
     });
 
-    /*
+   /*
     * Make the header images move on scroll
     */
-    window.addEventListener('scroll', function() {
-        var x = window.pageYOffset | document.body.scrollTop;
-        var m = document.getElementById("main"), c = m.style;
+    window.addEventListener('scroll', function () {
+        var x = window.pageYOffset || document.body.scrollTop;
+        var main = document.getElementById("main");
+        var mainStyle = main.style;
         
-        c.backgroundPosition = '100% ' + parseInt(-x/3) + 'px' + ', 0%, center top';
+        mainStyle.backgroundPosition = '100% ' + parseInt(-x/3) + 'px' + ', 0%, center top';
     });
 });
