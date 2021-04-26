20fda8e6781fa17f81da1b1e76aece3a51420af8
sylhare <sylhare@outlook.com>
2021-04-26 12:11:27 -0400

    Fix flickering on auto theme

--- assets/js/partials/dark-mode.js
+++ assets/js/partials/dark-mode.js
@@ -1,22 +1,24 @@
-// There are two color palettes on CSS, one for 'data-theme === light' and one for 'data-theme === dark'.
-// Initially the script check if a theme is set in session storage and alternatively listens to a MediaQuery callback looking for "prefers-color-scheme: dark".
+/*
+ * There are two color palettes on CSS for the data-theme: 'light' and 'dark'.
+ * Initially the script check if a theme is set in session storage and alternatively listens to a MediaQuery callback looking for "prefers-color-scheme: dark".
+ */
 
-// HTML of the dark theme button
+
 const themeButton = {
     'light': '<i class="fas fa-adjust" aria-hidden="true"></i>',
     'dark': '<i class="fas fa-adjust fa-rotate-180" aria-hidden="true"></i>'
 }
 
-// change theme
+const currentTheme = () => sessionStorage.getItem('theme')
+
 function setMode(theme) {
     document.documentElement.setAttribute('data-theme', theme)
     sessionStorage.setItem('theme', theme)
     document.getElementById('theme-toggle').innerHTML = themeButton[theme]
 }
 
-// theme switcher button callback
 function themeToggle() {
-    let sessionPrefers = sessionStorage.getItem('theme')
+    let sessionPrefers = currentTheme()
     if (sessionPrefers === 'light') {
         setMode('dark')
     } else {
@@ -26,14 +28,16 @@ function themeToggle() {
 
 window.onload = function bootstrapTheme() {
     if (isAutoTheme) {
-        // Load browser's preference
-        let browserPrefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-        browserPrefersDark.addEventListener('change', () => {
-            sessionStorage.setItem('theme', browserPrefersDark.matches ? 'dark' : 'light')
-        });
+        if (!currentTheme()) {
+            // Load browser's preference
+            let browserPrefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+            browserPrefersDark.addEventListener('change', () => {
+                if (browserPrefersDark.matches) sessionStorage.setItem('theme', 'dark')
+            });
+        }
 
         // Load theme
-        let sessionPrefers = sessionStorage.getItem('theme')
+        let sessionPrefers = currentTheme()
         setMode(sessionPrefers ? sessionPrefers : 'light')
     }
 }
