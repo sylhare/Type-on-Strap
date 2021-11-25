24b2d5568e2ca7ccf8fa8e9389ba40b6513092c3
sylhare <sylhare@outlook.com>
2021-11-25 09:33:43 -0500

    Update Cookie

--- assets/js/partials/cookie_consent_init.js
+++ assets/js/partials/cookie_consent_init.js
@@ -6,7 +6,7 @@ function createCookie(name, value, days) {
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
     expires = "; expires=" + date.toUTCString();
   }
-  document.cookie = name + "=" + value + expires + "; path=/";
+  document.cookie = `${name}=${value}${expires}; path=/`;
 }
 
 function readCookie(name) {
