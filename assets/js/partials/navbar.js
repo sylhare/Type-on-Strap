c9fd8ee73e2426dbbe4e3efa63627636811b311b
sylhare <sylhare@outlook.com>
2022-01-31 09:46:55 -0500

    Use const instead of var

--- assets/js/partials/navbar.js
+++ assets/js/partials/navbar.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
   /*
    * Display the menu items on smaller screens
    */
-  var pull = document.getElementById('pull');
-  var menu = document.querySelector('nav ul');
+  const pull = document.getElementById('pull');
+  const menu = document.querySelector('nav ul');
 
   ['click', 'touch'].forEach(function (e) {
     pull.addEventListener(e, function () {
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
    * Make the header images move on scroll
    */
   window.addEventListener('scroll', function () {
-    var offset = -(window.scrollY || window.pageYOffset || document.body.scrollTop) / 3;
+    const offset = -(window.scrollY || window.pageYOffset || document.body.scrollTop) / 3;
     document.getElementById("main").style.backgroundPosition = '100% ' + (offset - 50) + 'px' + ', 0%, center top';
   });
 });
